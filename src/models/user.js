783c10d5f5ff0d6a4fcbbcdceff385cc1fe49f28
Monika Filipowska <>
2019-07-08 15:33:33 +0200

    routers

---
+++ src/models/user.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		trim: true,
+		required: true
+	},
+	email: {
+		type: String,
+		trim: true,
+		lowercase: true,
+		required: true,
+		validate(value) {
+			if (!validator.isEmail(value)) {
+				throw new Error('Email is invalid');
+			}
+		}
+	},
+	age: {
+		type: Number,
+		validate: 0,
+		validate(value) {
+			if (value < 0) {
+				throw new Error('Age must be a positive number');
+			}
+		}
+	},
+	password: {
+		type: String,
+		required: true,
+		trim: true,
+		minlength: 7,
+		validate(value) {
+			if (value.toLowerCase().includes('password')) {
+				throw new Error('your password cannot contain password word');
+			}
+		}
+	}
+});
+
+userSchema.pre('save', async function(next) {
+	const user = this;
+
+	if (user.isModified('password')) {
+		user.password = await bcrypt.hash(user.password, 8);
+	}
+
+	next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
