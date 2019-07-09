277bc5d7248830948d583cbe68b3b7770d022f48
Monika Filipowska <>
2019-07-09 10:56:11 +0200

    authentication

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 require('./db/mongoose.js');
-const User = require('./models/user.js');
-const Task = require('./models/task.js');
-const userRouter = require('./routers/user.js');
-const taskRouter = require('./routers/task.js');
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// app.use((req, res, next) => {
+// 	if (req.method === 'GET') {
+// 		res.send('GET requests are disabled');
+// 	} else {
+// 		next();
+// 	}
+// });
+
+// app.use((req, res, next) => {
+// 	res.status(503).send('503: Website is temporarily unavailable');
+// });
 
 app.use(express.json());
 app.use(userRouter);
