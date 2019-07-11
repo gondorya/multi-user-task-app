291e594d53514b051df28c3bd53aa4d0e7f0d79e
Monika Filipowska <>
2019-07-11 15:28:49 +0200

    filters for tasks

--- src/models/user.js
+++ src/models/user.js
@@ -2,54 +2,60 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./task');
 
-const userSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		trim: true,
-		required: true
-	},
-	email: {
-		type: String,
-		unique: true,
-		trim: true,
-		lowercase: true,
-		required: true,
-		validate(value) {
-			if (!validator.isEmail(value)) {
-				throw new Error('Email is invalid');
+const userSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			trim: true,
+			required: true
+		},
+		email: {
+			type: String,
+			unique: true,
+			trim: true,
+			lowercase: true,
+			required: true,
+			validate(value) {
+				if (!validator.isEmail(value)) {
+					throw new Error('Email is invalid');
+				}
 			}
-		}
-	},
-	age: {
-		type: Number,
-		validate: 0,
-		validate(value) {
-			if (value < 0) {
-				throw new Error('Age must be a positive number');
+		},
+		age: {
+			type: Number,
+			validate: 0,
+			validate(value) {
+				if (value < 0) {
+					throw new Error('Age must be a positive number');
+				}
 			}
-		}
-	},
-	password: {
-		type: String,
-		required: true,
-		trim: true,
-		minlength: 7,
-		validate(value) {
-			if (value.toLowerCase().includes('password')) {
-				throw new Error('your password cannot contain password word');
+		},
+		password: {
+			type: String,
+			required: true,
+			trim: true,
+			minlength: 7,
+			validate(value) {
+				if (value.toLowerCase().includes('password')) {
+					throw new Error('your password cannot contain password word');
+				}
 			}
-		}
-	},
-	tokens: [
-		{
-			token: {
-				type: String,
-				required: true
+		},
+		tokens: [
+			{
+				token: {
+					type: String,
+					required: true
+				}
 			}
-		}
-	]
-});
+		]
+	},
+	{
+		timestamps: true
+	}
+);
 
 userSchema.virtual('tasks', {
 	ref: 'Task',
@@ -97,6 +103,15 @@ userSchema.pre('save', async function(next) {
 	if (user.isModified('password')) {
 		user.password = await bcrypt.hash(user.password, 8);
 	}
+
+	next();
+});
+
+// Delete user tasks when user is removed
+
+userSchema.pre('remove', async function(next) {
+	const user = this;
+	await Task.deleteMany({ owner: user._id });
 
 	next();
 });
